7db2bfeaf3cdf52e60ccc2efe37339e3084fbed4
Johannes Pelto-Piri <johannes.peltopiri@gmail.com>
2022-11-06 19:49:24 +0100

    more tests

--- test/api-stack.test.ts
+++ test/api-stack.test.ts
@@ -1,11 +1,11 @@
 
 import { App } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
-import { ApiStack } from "../lib/api-stack";
+import { ApiPipelineStack } from "../lib/api-pipeline";
 
-test('ApiStack snapshot', () => {
+test('Pipeline snapshot', () => {
   const app = new App();
-  const pipeline = new ApiStack(app, 'Api', {});
+  const pipeline = new ApiPipelineStack(app, 'Pipeline', {});
   const template = Template.fromStack(pipeline);
   expect(template.toJSON()).toMatchSnapshot();
 });
